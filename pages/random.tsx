917f6fc123e212730aaacff383f3f9dfaeaf8f0e
Nick Vitale <ntxnick@aol.com>
2021-12-12 00:56:22 -0600

    added Chakra-ui

--- pages/random.tsx
+++ pages/random.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next'
+import { Text, Heading, VStack } from '@chakra-ui/react'
 
 const Random: NextPage = () => {
   return (
-    <h1> 
-        Hit the random page!! it works!!!!
-    </h1>
-  )
+    <VStack>
+    <Heading>
+      This is the heading
+    </Heading>
+    <Text> 
+        Testing out Chakra-ui!!
+    </Text>
+    </VStack>
+  );
 }
 
 export default Random
