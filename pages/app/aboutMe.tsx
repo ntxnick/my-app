7a0e164fa9b593b6f4e639fd9e2e43a7bd0f98aa
Nick Vitale <ntxnick@aol.com>
2021-12-11 18:52:52 -0600

    testing out the pages

---
+++ pages/app/aboutMe.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const aboutMe: NextPage = () => {
+  return (
+  <h1>Hit the about me!</h1>
+  )
+}
+
+export default aboutMe
---
+++ pages/random.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Random: NextPage = () => {
+  return (
+    <h1> 
+        Hit the random page!! it works!!!!
+    </h1>
+  )
+}
+
+export default Random
